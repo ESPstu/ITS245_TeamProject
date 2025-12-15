1d4b52246a4ef2a58e0bd189810198ce938e038c
Issac Loera <iloera@pnw.edu>
2025-12-14 21:40:43 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms;
 
-namespace _245_Proj_Prototype
+namespace ITS245_Project
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            Application.Run(new Forms.FrmSelectPatient());
         }
     }
 }
